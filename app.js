2906d3ce18709aae1bd226ebc9e3459888f664c7
jill.tu <jilltu330@gmail.com>
2021-06-18 16:10:35 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 const express = require("express")
+const mongoose = require('mongoose')
+
 const app = express()
 const PORT = 3000
 
-app.get('/', (req, res)=>{
+mongoose.connect('mongodb://localhost/expense-tracker', { useNewUrlParser: true, useUnifiedTopology: true })
+
+const db = mongoose.connection
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
+app.get('/', (req, res) => {
   res.send('This is a Expense Tracker')
 })
 
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
   console.log(`App is running on http://localhost:${PORT}`)
 })
