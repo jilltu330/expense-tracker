746a0850aba526fac65886e01b91f7484dd29bac
jill.tu <jilltu330@gmail.com>
2021-06-19 15:18:29 +0800

    feat: modify index page ui

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require("express")
 const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
 
+const Record = require('./models/record')
+const Category = require('./models/category')
+
 const app = express()
 const PORT = 3000
 
@@ -17,9 +20,55 @@ db.once('open', () => {
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
+
 app.get('/', (req, res) => {
-  res.render('index')
+  const totalAmount = 0
+  const categories = [
+    {
+      "name": "家居物業",
+      "icon": "fas fa-home"
+    },
+    {
+      "name": "交通出行",
+      "icon": "fas fa-shuttle-van"
+    },
+    {
+      "name": "休閒娛樂",
+      "icon": "fas fa-grin-beam"
+    },
+    {
+      "name": "餐飲食品",
+      "icon": "fas fa-utensils"
+    },
+    {
+      "name": "其他",
+      "icon": "fas fa-pen"
+    }
+  ]
+  const records = [
+    {
+      "name": "買菜",
+      "category": "餐飲食品",
+      "date": "2021-05-24",
+      "amount": 680
+    },
+    {
+      "name": "機車加油",
+      "category": "交通出行",
+      "date": "2021-05-28",
+      "amount": 100
+    },
+    {
+      "name": "桌遊",
+      "category": "休閒娛樂",
+      "date": "2021-05-30",
+      "amount": 450
+    },
+  ]
+  res.render('index', { records, categories })
+
 })
+
 
 app.listen(PORT, () => {
   console.log(`App is running on http://localhost:${PORT}`)
