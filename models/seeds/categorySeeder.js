cf3add02d1fa835d7603487e794b0daf97cf1399
Jill <jilltu330@gmail.com>
2021-06-20 21:33:34 +0800

    refactor: modify mongoose config

--- models/seeds/categorySeeder.js
+++ models/seeds/categorySeeder.js
@@ -1,15 +1,8 @@
-const mongoose = require('mongoose')
-
-const Category= require('../category')
+const Category = require('../category')
 const { categories } = require('../../categories.json')
+const db = require('../../config/mongoose')
 
-mongoose.connect('mongodb://localhost/expense-tracker', { useNewUrlParser: true, useUnifiedTopology: true })
-const db = mongoose.connection
-db.on('error', () => {
-  console.log('mongodb error!')
-})
 db.once('open', () => {
-  console.log('mongodb connected!')
   Category.create(categories)
   console.log('done')
 })
